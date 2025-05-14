67adcbec46646fbfe38e4e0bfcf10c57c47c0e65
jayykayy98 <105195336+jayykayy98@users.noreply.github.com>
2025-05-14 18:47:09 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,58 +40,58 @@ document.getElementById("toggle-content").addEventListener("click", function () 
  *  Timer Countdown
   ======================================================= */
 
-// function setupCountdown(campaignSelector, startTimeMillis, endTimeMillis) {
-//     var second = 1000;
-//     var minute = second * 60;
-//     var hour = minute * 60;
-//     var day = hour * 24;
+function setupCountdown(campaignSelector, startTimeMillis, endTimeMillis) {
+    var second = 1000;
+    var minute = second * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
 
-//     function calculateRemaining() {
-//         var now = new Date().getTime();
-//         return now >= startTimeMillis && now < endTimeMillis ? endTimeMillis - now : 0;
-//     }
+    function calculateRemaining() {
+        var now = new Date().getTime();
+        return now >= startTimeMillis && now < endTimeMillis ? endTimeMillis - now : 0;
+    }
 
-//     var didRefresh = false;
-//     var previousGap = calculateRemaining();
+    var didRefresh = false;
+    var previousGap = calculateRemaining();
 
-//     function countdown() {
-//         var gap = calculateRemaining();
-//         var shouldRefresh = previousGap > day && gap <= day || previousGap > 0 && gap === 0;
+    function countdown() {
+        var gap = calculateRemaining();
+        var shouldRefresh = previousGap > day && gap <= day || previousGap > 0 && gap === 0;
 
-//         previousGap = gap;
+        previousGap = gap;
 
-//         var textDay = Math.floor(gap / day);
-//         var textHour = Math.floor((gap % day) / hour);
-//         var textMinute = Math.floor((gap % hour) / minute);
-//         var textSecond = Math.floor((gap % minute) / second);
+        var textDay = Math.floor(gap / day);
+        var textHour = Math.floor((gap % day) / hour);
+        var textMinute = Math.floor((gap % hour) / minute);
+        var textSecond = Math.floor((gap % minute) / second);
 
-//         if (document.querySelector(campaignSelector + ' .timer')) {
-//             document.querySelector(campaignSelector + ' .day').innerText = textDay;
-//             document.querySelector(campaignSelector + ' .hour').innerText = textHour;
-//             document.querySelector(campaignSelector + ' .minute').innerText = textMinute;
-//             document.querySelector(campaignSelector + ' .second').innerText = textSecond;
-//         }
+        if (document.querySelector(campaignSelector + ' .timer')) {
+            document.querySelector(campaignSelector + ' .day').innerText = textDay;
+            document.querySelector(campaignSelector + ' .hour').innerText = textHour;
+            document.querySelector(campaignSelector + ' .minute').innerText = textMinute;
+            document.querySelector(campaignSelector + ' .second').innerText = textSecond;
+        }
 
-//         if (shouldRefresh && !didRefresh) {
-//             didRefresh = true;
-//             setTimeout(function () {
-//                 window.location.reload();
-//             }, 30000 + Math.random() * 90000);
-//         }
-//     }
+        if (shouldRefresh && !didRefresh) {
+            didRefresh = true;
+            setTimeout(function () {
+                window.location.reload();
+            }, 30000 + Math.random() * 90000);
+        }
+    }
 
-//     countdown();
-//     setInterval(countdown, 1000);
-// }
+    countdown();
+    setInterval(countdown, 1000);
+}
 
-// document.addEventListener("DOMContentLoaded", function (event) {
-//     if (!document.querySelectorAll || !document.body.classList) {
-//         return;
-//     }
+document.addEventListener("DOMContentLoaded", function (event) {
+    if (!document.querySelectorAll || !document.body.classList) {
+        return;
+    }
 
-// });
+});
 
-// setupCountdown(".campaign-0", new Date().getMilliseconds(), 1924920000000);
+setupCountdown(".campaign-0", new Date().getTime(), new Date('2025-09-14T00:00:00').getTime());
 
 
 
